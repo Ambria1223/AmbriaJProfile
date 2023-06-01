6e4287018daac70accd29b5ab8c6f340f8fb7521
Ambria Johnson <1223ambria@gmail.com>
2023-06-01 01:11:48 -0400

    added contact component

--- assets/src/App.js
+++ assets/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
 import {Header,Footer} from './components/common';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
    <Header/>
+
+   <Router>
+    <Routes>
+      <Route path="/" About >
+        About
+      </Route>
+      <Route path="/portfolio">
+        Portfolio  
+      </Route>
+    </Routes>
+   </Router>
    <br />
    <br />
    <br />
--- assets/src/components/common/navbar/Navbar.jsx
+++ assets/src/components/common/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar () {
     
     return (
         <section className="navbar">
-      <a href="/" className="navbar-item">About Me</a>
+      <a href="/" className="navbar-item">About</a>
       <a href="/about" className="navbar-item">Portfolio</a>
       <a href="/portfolio" className="navbar-item">Contact</a>
       <a href="/shop" className="navbar-item">Resume</a>
